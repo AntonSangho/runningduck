ac13169cc0cd1a2c3a8125b68dd30fab950e2a8c
anton <sanghoemail@gmail.com>
2025-03-02 17:56:23 +0900

    회전시 아날로그 최대값으로 변경

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 input.onButtonPressed(Button.A, function () {
     pins.analogWritePin(AnalogPin.P1, 0)
-    pins.analogWritePin(AnalogPin.P2, 108)
+    pins.analogWritePin(AnalogPin.P2, 1023)
 })
 input.onButtonPressed(Button.AB, function () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 0)
 })
 input.onButtonPressed(Button.B, function () {
-    pins.analogWritePin(AnalogPin.P1, 701)
+    pins.analogWritePin(AnalogPin.P1, 1023)
     pins.analogWritePin(AnalogPin.P2, 0)
 })
 let Lcal = 0
@@ -16,14 +16,10 @@ pins.digitalWritePin(DigitalPin.P2, 0)
 basic.forever(function () {
     Lcal = pins.analogReadPin(AnalogPin.P0)
     if (Lcal > 500) {
-        basic.showIcon(IconNames.Heart)
-        basic.pause(200)
         pins.digitalWritePin(DigitalPin.P1, 0)
         pins.digitalWritePin(DigitalPin.P2, 0)
     } else {
         pins.analogWritePin(AnalogPin.P1, 0)
-        pins.analogWritePin(AnalogPin.P2, 268)
-        basic.clearScreen()
-        basic.pause(200)
+        pins.analogWritePin(AnalogPin.P2, 120)
     }
 })
